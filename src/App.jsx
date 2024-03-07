1f8b270402b8160b9f64fbfb1ade14e0d9f403be
Minnael <minnaelcampelo@gmail.com>
2024-03-06 22:08:05 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+import Navbar from "./Components/Layout/Navbar/Navbar.jsx"
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <h1>teste</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import './styles.css'
+import logo from '../../../assets/sti-logo.png'
+
+function Navbar(){
+   return (
+      <nav className='navbar'>
+         <img src={logo} alt="STI"/>
+            <div className='container-navbar'>
+               <ul className='list'>
+                  <li className='item'>Início</li>
+                  <li className='item'>Quem somos</li>
+                  <li className='item'>O que fazemos</li>
+                  <li className='item'>Oportunidades</li>
+                  <li className='item'>Fale conosco</li>
+               </ul>
+            </div>
+      </nav>
+   )
+}
+
+export default Navbar

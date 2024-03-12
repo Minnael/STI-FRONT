749738c114230158002aa0cfa9deb976b1a1a7d3
Minnael <minnaelcampelo@gmail.com>
2024-03-11 22:51:30 -0300

    add update in solucao-sistema

--- src/Components/Pages/QueFazemos/QueFazemos.jsx
+++ src/Components/Pages/QueFazemos/QueFazemos.jsx
@@ -6,9 +6,17 @@ function QueFazemos(){
       <div>
          <Descricao/>
          <div className="container-solucoes">
-            <div className="solucoes">
                <h2>Soluções em sistemas</h2>
-            </div>
+               <div className="linha-vertical"></div>
+               <p>
+                  Conheça nossos Sistemas Integrados de Gestão (SIG), responsáveis por 
+                  proporcionar modernização e agilidade à UFRN e diversas instituições federais.<br/>
+                  
+                  <br/>Nossos Sistemas Integrados de Gestão (SIG) proporcionam segurança e
+                  agilidade aos processos administrativos, acadêmicos e de recursos humanos
+                  dentro da UFRN e de diversas instituições federais, que possuem acordo de
+                  cooperação conosco.
+               </p>
          </div>
          <div className="infraestrutura-redes"></div>
       </div>

555438f709eada5aa554c64748b60cf85f397dae
Minnael <minaelcampelo3@gmail.com>
2025-05-21 14:33:11 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/STI-FRONT', // coloque o nome do seu repositório aqui
   plugins: [react()],
 })
